76caa45fc8a8d0959741df1f61a6b5b7672d79a9
tdreid <trevor.d.reid@gmail.com>
2018-07-01 23:45:08 +0000

    Use minified script for popup

--- src/popup.js
+++ src/popup.js
@@ -1,19 +1,19 @@
 /* global chrome */
 
 document.body.onload = function() {
-  chrome.storage.sync.get("dstillr", function(items) {
+  chrome.storage.sync.get('dstillr', function(items) {
     if (!chrome.runtime.error) {
       console.log(items);
-      document.getElementById("text").value = items.dstillr;
+      document.getElementById('text').value = items.dstillr;
     }
   });
 };
 
-document.getElementById("set").onclick = function() {
-  var d = document.getElementById("text").value;
-  chrome.storage.sync.set({ "dstillr" : d }, function() {
+document.getElementById('set').onclick = function() {
+  var d = document.getElementById('text').value;
+  chrome.storage.sync.set({ dstillr: d }, function() {
     if (chrome.runtime.error) {
-      console.log("dstillr runtime error.");
+      console.log('dstillr runtime error.');
     }
   });
   window.close();
